4a93ba03bb9e4d74fb9ed6e8008a45d8e46e91b3
Kim Yongjun <sdg8856@gmail.com>
2023-12-04 14:58:44 +0900

    최종 결과

--- src/main/java/opensource/example/subject/controller/ActivityController.java
+++ src/main/java/opensource/example/subject/controller/ActivityController.java
@@ -29,114 +29,6 @@ public class ActivityController {
         this.activityRepository = activityRepository;
     }
 
-
-
-    /*@GetMapping("/searchAi")
-    public ResponseEntity<List<Activity>> searchActivities(
-            @RequestParam(required = false) String gugunNm,
-            @RequestParam(required = false) String sidoNm,
-            @RequestParam(required = false) String srvcClCode,
-            @RequestParam(required = false) String yngbgsPosblAt,
-            @RequestParam(required = false) String adultPosblAt,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate possibleStartDate,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate possibleEndDate) {
-
-        Specification<Activity> spec = (root, query, cb) -> {
-            List<Predicate> predicates = new ArrayList<>();
-
-            // Adding the predicate for progrmSttusSe to be "모집중" directly
-            predicates.add(cb.equal(root.get("progrmSttusSe"), "모집중"));
-
-            if (gugunNm != null) {
-                predicates.add(cb.equal(root.get("gugunNm"), gugunNm));
-            }
-            if (sidoNm != null) {
-                predicates.add(cb.equal(root.get("sidoNm"), sidoNm));
-            }
-            if (srvcClCode != null) {
-                predicates.add(cb.equal(root.get("srvcClCode"), srvcClCode));
-            }
-            if (yngbgsPosblAt != null) {
-                predicates.add(cb.equal(root.get("yngbgsPosblAt"), yngbgsPosblAt));
-            }
-            if (adultPosblAt != null) {
-                predicates.add(cb.equal(root.get("adultPosblAt"), adultPosblAt));
-            }
-
-            if (possibleStartDate != null && possibleEndDate != null) {
-                Predicate dateOverlap = cb.or(
-                        cb.and(
-                                cb.lessThanOrEqualTo(root.get("progrmBgnde"), possibleEndDate),
-                                cb.greaterThanOrEqualTo(root.get("progrmEndde"), possibleStartDate)
-                        ),
-                        cb.and(
-                                cb.lessThanOrEqualTo(root.get("progrmBgnde"), possibleStartDate),
-                                cb.greaterThanOrEqualTo(root.get("progrmEndde"), possibleStartDate)
-                        ),
-                        cb.and(
-                                cb.lessThanOrEqualTo(root.get("progrmBgnde"), possibleEndDate),
-                                cb.greaterThanOrEqualTo(root.get("progrmEndde"), possibleEndDate)
-                        )
-                );
-                predicates.add(dateOverlap);
-            }
-
-            Predicate activityDurationPredicate = cb.notEqual(
-                    cb.diff(root.get("actEndTm"), root.get("actBeginTm")),
-                    0
-            );
-            predicates.add(activityDurationPredicate);
-
-            query.distinct(true); // 중복을 제거합니다.
-            return cb.and(predicates.toArray(new Predicate[0]));
-        };
-
-        List<Activity> result = activityRepository.findAll(spec);
-        return ResponseEntity.ok(result);
-    }
-
-    // 새로운 추천 API 메소드
-    @GetMapping("/recommendActivities")
-    public ResponseEntity<List<Activity>> recommendActivities(
-            @RequestParam(required = false) String gugunNm,
-            @RequestParam(required = false) String sidoNm,
-            @RequestParam(required = false) String srvcClCode,
-            @RequestParam(required = false) String yngbgsPosblAt,
-            @RequestParam(required = false) String adultPosblAt,
-            @RequestParam(required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate possibleStartDate,
-            @RequestParam(required = true) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate possibleEndDate,
-            @RequestParam(required = false) Long totalTime) {
-
-        // searchActivities 메소드를 호출하여 조건에 맞는 활동을 검색합니다.
-        List<Activity> activities = searchActivities(gugunNm, sidoNm, srvcClCode, yngbgsPosblAt, adultPosblAt, possibleStartDate, possibleEndDate).getBody();
-
-        // totalTime에 가장 근접하는 활동의 조합을 찾습니다.
-        List<Activity> recommendedActivities = findBestActivityCombination(activities, totalTime);
-
-        // 추천된 활동들을 반환합니다.
-        return ResponseEntity.ok(recommendedActivities);
-    }
-
-    private List<Activity> findBestActivityCombination(List<Activity> activities, long totalTime) {
-        Collections.shuffle(activities, new Random());
-
-        List<Activity> bestCombination = new ArrayList<>();
-        int currentTotalTime = 0;
-
-        for (Activity activity : activities) {
-            int activityDuration = activity.getActDuringTm();
-
-            // 새 활동을 추가했을 때 totalTime을 초과하지 않는 경우에만 추가합니다.
-            if (currentTotalTime + activityDuration <= totalTime) {
-                bestCombination.add(activity);
-                currentTotalTime += activityDuration;
-            }
-        }
-
-        return bestCombination;
-    }
-*/
-
     // 날짜 할당을 위한 함수 추가
     private LocalDate assignRandomDate(LocalDate start, LocalDate end) {
         long days = ChronoUnit.DAYS.between(start, end);
@@ -261,10 +153,6 @@ public class ActivityController {
             // 충족할 수 있는 조합이 없음을 알리는 빈 리스트 반환
             return ResponseEntity.ok(Collections.emptyList());
         }
-        /*if (totalDuration < totalTime) {
-            // 충족할 수 있는 조합이 없음을 알립니다.
-            return ResponseEntity.ok("해당 조건을 가지고는 총 봉사 시간을 충족할 수 있는 조합이 없습니다");
-        }*/
 
         // 정렬 로직: assignedDate가 빠른 순, 그 다음 actBeginTm이 빠른 순
         recommendedActivities.sort(Comparator.comparing(Activity::getAssignedDate)
@@ -311,13 +199,6 @@ public class ActivityController {
 
         return bestCombination;
     }
-
-
-
-
-
-
-
 
     @GetMapping("/search")
     public ResponseEntity<List<Activity>> searchActivities(
@@ -373,7 +254,5 @@ public class ActivityController {
         Page<Activity> resultPage = activityRepository.findAll(spec, pageable);  // 결과를 페이지네이션하여 받음
         return ResponseEntity.ok(resultPage.getContent());  // 페이지의 내용을 반환
     }
-
-
 
 }
